4f92f9c9d7de83729082e34d27a5756d1f8d04b6
Hoai Truong <hoaitruong@Hoais-MacBook-Pro.local>
2018-06-05 21:31:01 +0700

    Html css basic

---
+++ basic.js
@@ -0,0 +1,14 @@
+console.log('hello world')
+console.log('x ', x)
+console.log('y ', y)
+console.log('z ', z)
+
+var x = '20'
+var y = x + 5 + 'hello'
+var z = 10 // false => null undefined 0
+if(z) {
+    console.log('have value')
+} else {
+    console.log('dont have value')
+}
+
