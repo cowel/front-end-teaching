91d62caaa866285c3c75aa03124e2f7f46eed7f8
Hoai Truong <hoaitruong@Hoais-MacBook-Pro.local>
2018-06-08 22:20:17 +0700

    Solves homeword and functions of array

--- html-0806/javascript.js
+++ html-0806/javascript.js
@@ -27,13 +27,57 @@ var getDatesFromDate = function (date, numOfDate = 7) {
   var valueDate = new Date(date).valueOf()
   var result = []
   for (var i = 0; i < numOfDate; i++) {
-    
+
     // next i date
     var nextDate = formatDate(new Date(valueDate + milliSecondDay * i))
     result.push(nextDate)
+    if( i === 3) return result
   }
   return result
 }
 
 console.log('7 dates ', getDatesFromDate(new Date(), 10))
 
+// Array
+
+var array = [1, 5, 7, 9]
+array.forEach((element, index) => {
+  console.log('element ', element)
+  if(index > 2) return
+});
+
+var number = array.filter(function (element) {
+  return element > 5 
+}) // => return Array
+
+console.log('number ', number)
+
+var family = {
+  quantity: 6,
+  names: ['Trang', 'Loan', 'Ngan', 'Hieu', 'Phong', 'Hoai'],
+  ages: [13, 15, 11, 45, 23, 12]
+}
+
+// Find an oldest person
+var findOldestPerson = function(family) {
+  var indexPerson = 0
+  for (var i = 1; i < family.quantity; i++) {
+    indexPerson = family.ages[i] > family.ages[i-1] ? i : indexPerson
+  }
+  return family.names[indexPerson]
+}
+
+console.log('findOldestPerson ', findOldestPerson(family))
+
+// Sum of people's age
+var sumAge = function(family) {
+  return family.ages.reduce(function (total, value) {
+    return total + value
+  })
+} // reduce return a value
+console.log('sumAge ', sumAge(family))
+
+var person = function(name, age) {
+  this.name = name;
+  this.age = age
+}
