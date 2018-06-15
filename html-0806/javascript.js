d39f15682458e4c037e3438d68152c373e706004
Hoai Truong <hoaitruong@Hoais-MacBook-Pro.local>
2018-06-15 18:06:38 +0700

    OOP

--- html-0806/javascript.js
+++ html-0806/javascript.js
@@ -189,9 +189,25 @@ var TodoList = function (todoLists) {
 }
 
 TodoList.prototype.createTodoList = function() {
+  var numOfDate = getNumberOfDate(new Date (2018, 5, 1))
+  var dateValueOf = new Date (2018, 5, 1).valueOf()
+  for (var i = 0; i < numOfDate; i++) {
+    var date = dateValueOf + i * milSecondDate
+    var text = ''
+    var isCompleted = false
+    if (checkDay(date)) {
+      text = 'Todo'
+      isCompleted = true
+    } 
+    this.list.push(new Todo(text, isCompleted, new Date (date).toISOString()))
+  }
+}
 
+TodoList.prototype.filterCompleted = function () {
+  return this.list.filter(todo => todo.isCompleted ? true : false)
 }
 
 var todoList = new TodoList([])
 todoList.createTodoList()
-todoList.list
+console.log('list ', todoList.list)
+console.log('filter  ', todoList.filterCompleted())
